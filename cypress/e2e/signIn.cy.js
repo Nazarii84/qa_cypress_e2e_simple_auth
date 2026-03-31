2059e18eb9c272ac9ce7d9f7855291f863ee6a5b
Nazarii Kaminskyi <nazarruda333@gmail.com>
2026-03-31 21:33:00 +0300

    add cypress login tests

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -5,7 +5,38 @@ describe('Sign In page', () => {
 
   });
 
-  it('', () => {
+  it('should open login page', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
 
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+
+    cy.get('button[type="submit"]').click();
+
+    cy.contains('You logged into a secure area!').should('be.visible');
+  });
+
+  it('should show error for invalid credentials', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+
+    cy.get('#username').type('wrongUser');
+    cy.get('#password').type('wrongPassword');
+
+    cy.get('button[type="submit"]').click();
+
+    cy.contains('Your username is invalid!').should('be.visible');
+  });
+
+  it('should logout successfully', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+
+    cy.get('#username').type('tomsmith');
+    cy.get('#password').type('SuperSecretPassword!');
+
+    cy.get('button[type="submit"]').click();
+
+    cy.contains('a', 'Logout').click();
+
+    cy.contains('You logged out of the secure area!').should('be.visible');
   });
 });
